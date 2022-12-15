222bb06ab9bbf4c4fe91f6199f8eb9a23bd9e2fb
vatsalvasani <vatsalvasani22882@gmail.com>
2022-12-15 17:56:52 +0530

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,56 +1,30 @@
-// C++ program for implementation of
-// selection sort
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+void Selection_Sort(vector<int> &arr){
+    //simple idea is to manage two array 1 is sorted and one in unsorted Time Complexity In All The Case Is O(n^2)
+    //run loop till n-1
+    for(int i=0;i<arr.size()-1;i++)
+    {
+        //take remaining array's first element as min
+        int min = arr[i];
+        //run a loop till n from i+1
+        for(int j=i+1;j<arr.size();j++)
+        {
+            if(arr[j]<min)
+            {
+                swap(arr[j],arr[i]);
+            }
+        }
+    }
+    for(int i=0;i<arr.size();i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
 
-//Swap function
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
 }
-
-void selectionSort(int arr[], int n)
-{
-	int i, j, min_idx;
-
-	// One by one move boundary of
-	// unsorted subarray
-	for (i = 0; i < n-1; i++)
-	{
-	
-		// Find the minimum element in
-		// unsorted array
-		min_idx = i;
-		for (j = i+1; j < n; j++)
-		if (arr[j] < arr[min_idx])
-			min_idx = j;
-
-		// Swap the found minimum element
-		// with the first element
-		if(min_idx!=i)
-			swap(&arr[min_idx], &arr[i]);
-	}
+int main(){
+    vector<int> arr={1,2,3,4,2,111,12343,34};
+    Selection_Sort(arr);
+    return 0;
 }
-
-//Function to print an array
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i=0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-// Driver program to test above functions
-int main()
-{
-	int arr[] = {64, 25, 12, 22, 11};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	selectionSort(arr, n);
-	cout << "Sorted array: \n";
-	printArray(arr, n);
-	return 0;
-}
-// This is code is contributed by rathbhupendra
